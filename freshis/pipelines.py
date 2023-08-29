d8463751cbdc436b91b7736394a356295a991633
x-j <16541933+x-j@users.noreply.github.com>
2023-08-29 04:45:10 +0200

    handle dual csv output

--- freshis/pipelines.py
+++ freshis/pipelines.py
@@ -1,13 +1,20 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import os
 
 
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
 class FreshisPipeline:
+
+    def open_spider(self, spider):
+        feeds = spider.settings.getdict('FEEDS')
+        num_csv_feeds = sum([uri.endswith('.csv') for uri in feeds.keys()])
+        if num_csv_feeds > 1 and os.path.exists("out/fresh-info.csv"):
+            outsize = len(os.listdir('out'))
+            newname = f"old-fresh-info-{str(outsize)}.csv"
+            spider.logger.info(f"Alternate csv output feed detected. I am renaming the previous fresh-info.csv to {newname}")
+            os.rename('out/fresh-info.csv', 'out/'+newname)
+
+
     def process_item(self, item, spider):
         return item
--- freshis/settings.py
+++ freshis/settings.py
@@ -84,9 +84,9 @@ FAKEUSERAGENT_PROVIDERS = [
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "freshis.pipelines.FreshisPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "freshis.pipelines.FreshisPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
