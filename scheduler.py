d3811fd8684155fc410fe5a3e2361d88d3650329
x-j <16541933+x-j@users.noreply.github.com>
2023-08-29 03:27:28 +0200

    logger for scheduler

--- scheduler.py
+++ scheduler.py
@@ -9,25 +9,24 @@ import logging
 
 class Scheduler:
 
-    def __init__(self, command, every):
+    def __init__(self, command: str, every: int):
+        self.logger = logging.getLogger(type(self).__name__)
         self.command = command
-        print(time.strftime('%Y-%m-%d %T [scheduler]', time.localtime()), 'Scheduler initialised.\nCommand: '+command+" | every: "+str(every)+" mins.")
+        self.logger.info('Scheduler initialised.\t| command: '+command+' | every: '+str(every)+' mins.')
         schedule.every(every).minutes.do(lambda: os.system(command))
 
     def run(self):
-        print(time.strftime('%Y-%m-%d %T [scheduler]', time.localtime()), "Initial run...")
+        self.logger.info('Initial run...')
         os.system(self.command)
         while True:
-            print(time.strftime('%Y-%m-%d %T [scheduler]', time.localtime()),"Running...")
             schedule.run_pending()
-            timeout = 60
-            
+            timeout = 59
             time.sleep(timeout)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Scheduler')
     parser.add_argument('command', type=str, help='OS command to be scheduled')
-    parser.add_argument('--every', type=int, help='How often to run the scheduled command (in minutes)', dest='every', default=60)
+    parser.add_argument('--every', type=int, help='how often to run the scheduled command (in minutes)', dest='every', default=60)
 
     args = vars(parser.parse_args())
     Scheduler(command = args['command'], every=args['every']).run()
